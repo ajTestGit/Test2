8351fcb45adf1df35607e08038533273019b6b3c
abram <azhuzhunas@juno.com>
2015-06-21 12:48:09 -0600

    commit 22

--- Test/src/main/SiebelDao.java
+++ Test/src/main/SiebelDao.java
@@ -8,6 +8,11 @@ public class SiebelDao {
 		String testStr2 = "abc2";
 		System.out.println(testStr2);
 
+		String testStr1 = "abc1";
+		System.out.println(testStr1);
+		String testStr11 = "abc11";
+		System.out.println(testStr11);
+
 	}
 	
 }
